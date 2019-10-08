4a41731f18743124acd5f853ad94559ffd26fd95
dipeshkolhe <dipeshkolhe@gmail.com>
2019-10-08 17:21:42 +0530

    Code for reading data from csv files and create data frames

---
+++ src/main/scala/com/dipesh/movielens/MovieLensInsights.scala
@@ -0,0 +1,31 @@
+package com.dipesh.movielens
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.DataFrame
+
+object MovieLensInsights{
+
+    val logger = LoggerFactory.getLogger("MovieLensInsights")
+
+    def readCSV(spark: SparkSession, path: String, header: Boolean, inferSchema: Boolean): DataFrame = {
+        spark.read
+            .format("csv")
+            .option("header", header.toString)
+            .option("inferSchema", inferSchema.toString)
+            .load(path)
+    }
+
+    def main(args: Array[String]): Unit = {
+        val spark = SparkSession.builder.appName("MovieLensInsights").master("local").getOrCreate
+        
+        val moviesDF = readCSV(spark, "ml-latest-small/movies.csv", true, true)
+        val linksDF = readCSV(spark, "ml-latest-small/links.csv", true, true)
+        val ratingsDF = readCSV(spark, "ml-latest-small/ratings.csv", true, true)
+        val tagsDF = readCSV(spark, "ml-latest-small/tags.csv", true, true)
+
+        
+    }
+}
